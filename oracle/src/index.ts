124b97ab71ad6ddfbf6c98f89e023d0ceb2ccefa
t4top <temitope@yahoo.co.jp>
2022-12-08 20:21:30 +0000

    add Koa, an http server, for oracle

--- oracle/src/index.ts
+++ oracle/src/index.ts
@@ -1,3 +1,11 @@
-console.log("index.ts");
+import Koa from "koa";
 
-export {};
+// set server listen port. Default is 3000
+const DEFAULT_PORT: number = 3000;
+const port = process.env.PORT || DEFAULT_PORT;
+
+const app = new Koa();
+
+// start listening for incoming requests
+console.log(`server listening of port ${port}`);
+app.listen(port);
