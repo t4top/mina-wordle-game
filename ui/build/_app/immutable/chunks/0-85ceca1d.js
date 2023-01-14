ef048b3ca424b00086ec5856e76c62596ebb16b8
t4top <temitope@yahoo.co.jp>
2023-01-14 18:39:36 +0100

    remove build artefacts

--- ui/build/_app/immutable/chunks/0-85ceca1d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-723b139b.js";const e=!0;export{t as component,e as server};
--- ui/build/_app/immutable/chunks/1-0683818e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-067a22e6.js";export{t as component};
